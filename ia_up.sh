beb8e1bf6f303c51e810b1822f4d740b999cedf6
X.C. <dionkxk7night@gmail.com>
2025-09-07 21:49:29 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-2.lolinet.com/firmware/lenowow/"
-COLLECTION="lenovo-mobile-devices-firmware-2014-2019"
+ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/nec/"
+COLLECTION="nec-mobile-devices-firmware-2020-2021"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
