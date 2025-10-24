06e1372312bd67212fcfbf096f0516cbca68c82b
X.C. <dionkxk7night@gmail.com>
2025-10-25 07:34:31 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-2.lolinet.com/firmware/lenomola/2019/kane/"
-COLLECTION="motolola-mobile-devices-firmware-2019-kane"
+ROOT_URL="https://mirrors-obs-2.lolinet.com/firmware/lenomola/2019/river/"
+COLLECTION="motolola-mobile-devices-firmware-2019-river"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
