437214a5d7da8d3840448abadc2737a2609a79e9
X.C. <dionkxk7night@gmail.com>
2025-11-10 19:38:22 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/berlin/"
-COLLECTION="motolola-mobile-devices-firmware-2021-berlin"
+ROOT_URL="https://mirrors-obs-2.lolinet.com/firmware/lenomola/2018/deen/"
+COLLECTION="motolola-mobile-devices-firmware-2018-deen"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
