e8710c02cda25ebf21ec9196c7c011db93625773
X.C. <dionkxk7night@gmail.com>
2025-11-12 19:17:59 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/nio/"
-COLLECTION="motolola-mobile-devices-firmware-2021-nio"
+ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/tonga/"
+COLLECTION="motolola-mobile-devices-firmware-2021-tonga"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
