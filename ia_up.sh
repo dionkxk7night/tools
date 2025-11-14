cd963b9741b986405bf08f55d59eb8569dbd13fe
X.C. <dionkxk7night@gmail.com>
2025-11-14 16:17:52 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/cofud/"
-COLLECTION="motolola-mobile-devices-firmware-2021-cofud"
+ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/caprip/"
+COLLECTION="motolola-mobile-devices-firmware-2021-caprip"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
