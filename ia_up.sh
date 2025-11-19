fd78608fdf70549055ff3dc4b5d90e492493ea63
X.C. <dionkxk7night@gmail.com>
2025-11-20 01:36:55 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/ellis/"
-COLLECTION="motolola-mobile-devices-firmware-2021-ellis"
+ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/ibiza/"
+COLLECTION="motolola-mobile-devices-firmware-2021-ibiza"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
