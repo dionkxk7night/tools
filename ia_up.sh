fc1a5b2a9856a865c64c3e49875aa4a9cb817bbf
X.C. <dionkxk7night@gmail.com>
2025-11-23 01:45:52 +0800

    Update ia_up.sh

--- ia_up.sh
+++ ia_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ia_stream_upload_checked.sh
 
-ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/cypfq/"
-COLLECTION="motolola-mobile-devices-firmware-2021-cypfq"
+ROOT_URL="https://mirrors-obs-1.lolinet.com/firmware/lenomola/2021/corfur/"
+COLLECTION="motolola-mobile-devices-firmware-2021-corfur"
 LIST_FILE="$HOME/ia_project/file_list.txt"
 LOG_DIR="$HOME/ia_project/logs"
 TMP_DIR="$HOME/ia_project/tmp"
